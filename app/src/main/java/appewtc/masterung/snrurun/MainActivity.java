b45b4c419a43a90743925fc70b15bf44748e1a95
natsu <googgig1999@gmail.com>
2016-05-17 01:16:31 -0700

    Initial commit

---
+++ app/src/main/java/appewtc/masterung/snrurun/MainActivity.java
@@ -0,0 +1,25 @@
+package appewtc.masterung.snrurun;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private MyManage myManage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myManage = new MyManage(MainActivity.this);
+
+    }   // Main Method
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this, SignUp.class));
+    }
+
+}   // Main Class นี่คือ คลาสหลัก
